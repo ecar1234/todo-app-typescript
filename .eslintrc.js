ac1b82ddc3b8e781fbc1fd1b6aafd8229a3fc89a
jk <ecar1234@gmail.com>
2021-02-01 23:21:41 +0900

    typescript react eslint/setting

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+      'eslint-plugin-react',
+      'eslint-plugin-react-hooks',
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:eslint-plugin-react/recommended',
+      'plugin:eslint-plugin-react-hooks/recommended',
+    ],
+  };
